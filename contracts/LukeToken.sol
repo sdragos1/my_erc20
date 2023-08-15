2f1b227c61fb5e31c092149b0e4a4ec9ead96c33
sdragos1 <dragos.suceveanu@epitech.eu>
2023-08-15 21:00:08 +0200

    feat: make token burnable

--- contracts/LukeToken.sol
+++ contracts/LukeToken.sol
@@ -1,15 +1,20 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.8.2 <0.9.0;
+pragma solidity ^0.8.17;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract LukeToken is ERC20Capped {
+contract LukeToken is ERC20, ERC20Capped, ERC20Burnable{
     address payable public owner;
 
     constructor(uint256 cap) ERC20("LukeToken", "LKT") ERC20Capped(cap * (10 ** decimals())) {
         owner = payable(msg.sender);
         _mint(owner, 7 * (10 ** 6) * (10 ** decimals()));
+    }
+
+    function _mint(address account, uint256 amount) internal virtual override(ERC20, ERC20Capped) {
+        _mint(account, amount);
     }
 }
